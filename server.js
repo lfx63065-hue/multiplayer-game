3cc02ed440424613113e17540969c9a44edd0fd5
Willy-fox0405 <wzwzwzwz63065@gmail.com>
2026-05-30 22:03:51 +0800

    prepare for deploy

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
